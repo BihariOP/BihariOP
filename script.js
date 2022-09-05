e22f682f59b589eb6c6267035f933e26a0c617d0
BihariOP <112867224+BihariOP@users.noreply.github.com>
2022-09-05 13:58:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Type sam");
+ var b=prompt("type 69");
+  	if(a== "sam" && b=="69")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
